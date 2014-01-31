3ab6fea15b506d14770952cb365c0c7277c5808d
Darong Mean <darong012@gmail.com>
2014-01-31 09:33:56 +0700

    [#1] levenshtein iterative method

--- project.clj
+++ project.clj
@@ -3,7 +3,8 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/core.match "0.2.1"]]
   :main ^:skip-aot khlp.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/khlp/word.clj
+++ src/khlp/word.clj
@@ -1,4 +1,5 @@
-(ns khlp.word)
+(ns khlp.word
+  (use [clojure.core.match :only (match)]))
 
 (defn words [text] (re-seq #"[a-z]+" (.toLowerCase text)))
 
@@ -20,6 +21,37 @@
            (min (+ 1 (levenshtein-recur0 s (- len-s 1) t len-t))
                 (+ 1 (levenshtein-recur0 s len-s t (- len-t 1)))
                 (+ cost (levenshtein-recur0 s (- len-s 1) t (- len-t 1)))))))
+
+(defn next-row [ch ch-distance word-t previous-row]
+  (loop [t word-t
+         idx 0
+         v1s [ch-distance]]
+    (if (seq t)
+      (let [cost (if (= ch (first t))
+                   0
+                   1)]
+        (recur (rest t)
+               (inc idx)
+               (conj v1s
+                     (min (+ 1 (last v1s))
+                          (+ 1 (get previous-row (inc idx)))
+                          (+ cost (get previous-row idx))))))
+      v1s)))
+
+
+(defn levenshtein-itr [s t]
+  (let [len-s (count s)
+        len-t (count t)]
+    (cond
+     (= len-s 0) [len-t]
+     (= len-t 0) [len-s]
+     :else (loop [idx 0
+                  v0 (vec (for [i (range (inc len-t))] i))]
+             (if (>= idx len-s)
+               v0
+               (recur (inc idx)
+                      (next-row (get s idx) (inc idx) t v0)))))))
+
 
 (defn levenshtein
   "Take two words and return how far they are.
