7a4dbe6515614e640d44abe747b7346e7c3322a3
Darong Mean <darong012@gmail.com>
2014-02-17 20:36:52 +0700

    [#1] test levenshtein method

--- test/khlp/core_test.clj
+++ test/khlp/core_test.clj
@@ -1,7 +1,61 @@
 (ns khlp.core-test
-  (:require [khlp.core :refer :all])
-  (:use midje.sweet))
+  (:use midje.sweet)
+  (:require [khlp.word :as word]))
+
+;; Util
+;; -----
+(defmacro time-ms [f]
+  `(let [start# (System/nanoTime)]
+    ~@f
+    (/ (- (System/nanoTime) start#) 1e6)))
+
+;; logic check
+;; ------------
+(tabular
+ (fact "recursive calculate levenshtein between two strings"
+      (word/levenshtein ?a ?b) => ?c)
+ ?a ?b ?c
+ "" "" 0
+ "" "sitting" 7
+ "kitten" "" 6
+ "kitten" "sitting" 3
+ "saturday" "sunday" 3)
+
+;; performance check
+;; -----------------
+(tabular
+ (fact "recursive calculate levenshtein distance between two strings should be between 0.03ms"
+      (time-ms (word/levenshtein ?a ?b)) => #(< 0 % 0.03))
+ ?a ?b
+ "" ""
+ "" "sitting"
+ "kitten" ""
+ "kitten" "sitting"
+ "saturday" "sunday"
+ "aaaaaawwwaaaaaaaaaauuuaaaaaaaaaa" "bbbbbbbbcccbbbbbbbnnnbbb")
 
 
-(fact "HELP Me! I passed"
-       0 => 0)
+;; logic check
+;; ------------
+(tabular
+ (fact "iterative calculate levenshtein between two strings"
+      (last (word/levenshtein-itr ?a ?b)) => ?c)
+ ?a ?b ?c
+ "" "" 0
+ "" "sitting" 7
+ "kitten" "" 6
+ "kitten" "sitting" 3
+ "saturday" "sunday" 3)
+
+;; performance check
+;; -----------------
+(tabular
+ (fact "iterative calculate levenshtein distance between two strings should be between 0.03ms"
+      (time-ms (word/levenshtein-itr ?a ?b)) => #(< 0 % 0.03))
+ ?a ?b
+ "" ""
+ "" "sitting"
+ "kitten" ""
+ "kitten" "sitting"
+ "saturday" "sunday"
+ "aaaaaawwwaaaaaaaaaauuuaaaaaaaaaa" "bbbbbbbbcccbbbbbbbnnnbbb")
